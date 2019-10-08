89d325e6e899d9c573a89dbd4e7d3ab171d760b7
John Rogers <john.rogers@softgames.de>
2019-10-08 18:04:11 +0200

    updated scraper to use abs path for windows compat

--- scraper.js
+++ scraper.js
@@ -1,70 +1,69 @@
-const puppeteer = require('puppeteer');
-const CREDS = require('./creds');
+const puppeteer = require("puppeteer");
+const CREDS = require("./creds");
 let wait = ms => new Promise((r, j) => setTimeout(r, ms));
 
 module.exports = {
-	scrape: async () => {
-		const browser = await puppeteer.launch({
-			headless: true
-		});
+  scrape: async () => {
+    const browser = await puppeteer.launch({
+      headless: true
+    });
 
-		const page = await browser.newPage();
+    const page = await browser.newPage();
 
-		await page.goto('https://www.facebook.com/login/');
+    await page.goto("https://www.facebook.com/login/");
 
-		const USERNAME_SELECTOR = '#email';
-		const PASSWORD_SELECTOR = '#pass';
-		const BUTTON_SELECTOR = '#loginbutton';
+    const USERNAME_SELECTOR = "#email";
+    const PASSWORD_SELECTOR = "#pass";
+    const BUTTON_SELECTOR = "#loginbutton";
 
-		await page.click(USERNAME_SELECTOR);
-		await page.keyboard.type(CREDS.username);
+    await page.click(USERNAME_SELECTOR);
+    await page.keyboard.type(CREDS.username);
 
-		await page.click(PASSWORD_SELECTOR);
-		await page.keyboard.type(CREDS.password);
+    await page.click(PASSWORD_SELECTOR);
+    await page.keyboard.type(CREDS.password);
 
-		await page.click(BUTTON_SELECTOR);
+    await page.click(BUTTON_SELECTOR);
 
-		await page.waitForNavigation();
+    await page.waitForNavigation();
 
-		console.log('Finished login...');
+    console.log("Finished login...");
 
-		await page.goto(
-			'https://developers.facebook.com/apps/755626824646698/instant-games/feedback/?business_id=1866079323706610'
-		);
-		await page.waitForNavigation();
+    await page.goto(
+      "https://developers.facebook.com/apps/755626824646698/instant-games/feedback/?business_id=1866079323706610"
+    );
+    await page.waitForNavigation();
 
-		console.log('Entering clickExportButton...');
+    console.log("Entering clickExportButton...");
 
-		const clickExportButton = async () => {
-			try {
-				const nodeList = document.querySelectorAll('[role="button"]');
-				const items = {};
+    const clickExportButton = async () => {
+      try {
+        const nodeList = document.querySelectorAll('[role="button"]');
+        const items = {};
 
-				for (let element of nodeList) {
-					if (element.innerText === 'Export to CSV') {
-						await element.click();
-						items.link = element.className; //what do?
-					}
-				}
-				return items;
-			} catch (err) {
-				throw err;
-			}
-		};
+        for (let element of nodeList) {
+          if (element.innerText === "Export to CSV") {
+            await element.click();
+            items.link = element.className; //what do?
+          }
+        }
+        return items;
+      } catch (err) {
+        throw err;
+      }
+    };
 
-		await page.evaluate(clickExportButton);
+    await page.evaluate(clickExportButton);
 
-		console.log('Setting download behaviour...');
-		await page._client.send('Page.setDownloadBehavior', {
-			behavior: 'allow',
-			downloadPath: './'
-		});
+    // console.log('Setting download behaviour...');
+    await page._client.send("Page.setDownloadBehavior", {
+      behavior: "allow",
+      downloadPath: __dirname
+    });
 
-		console.log('Download behaviour set...');
-		console.log('wait...');
-		await wait(10000);
-		console.log('download...');
-		console.log('Fin');
-		browser.close();
-	}
+    console.log("wait...");
+    await wait(10000);
+    console.log("download...");
+    console.log("Fin");
+    browser.close();
+  }
 };
